5f0a91e87bd590585a6a96eee1ca65e5f592a7ee
Tanvi Kapoor <tanvi.kapoor@C02G7X4PDRJ7.local>
2014-12-09 01:11:54 +0530

    first challenge solution

--- FirstChallenge/ViewController.swift
+++ FirstChallenge/ViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func mailButton(sender: AnyObject) {
+        
+        label.hidden = false
+        label.text = nameField.text
+        label.textColor = UIColor.blueColor()
+        nameField.text = ""
+        messageField.resignFirstResponder()
+        button.setTitle("Mail Sent", forState: UIControlState.Normal)
+    }
 
 }
 
