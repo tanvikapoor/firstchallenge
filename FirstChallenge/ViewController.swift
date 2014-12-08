ff30ad2048e2e39dee72cbcd478c3c9a7cf09eb2
Tanvi Kapoor <tanvi.kapoor@C02G7X4PDRJ7.local>
2014-12-09 00:33:19 +0530

    Initial Commit

---
+++ FirstChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstChallenge
+//
+//  Created by Tanvi Kapoor on 12/9/14.
+//  Copyright (c) 2014 Tanvi Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
